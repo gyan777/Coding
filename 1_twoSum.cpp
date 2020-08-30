083d74ee630a0a9e8a728c78bfa09935fb19fa2b
gyan777 <prashant.jain@gmail.com>
2020-08-30 15:07:31 -0700

    Working version with insert

--- 1_twoSum.cpp
+++ 1_twoSum.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
-use namespace std;
+//Given an array of integers nums and and integer target, return the indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-vector<int> twoSum (const vector<int>& numbers, const int& target) {
+//Add numbes to a map and look if the complement (or diff with the target is in the map.
+//note use of insert 
+
+using namespace std;
+
+vector<int> twoSum (const vector<int>& nums, const int& target) {
 
   vector<int> pair;
-  
-  for (int i=0; i<numbers.size(); i++) {
+  unordered_map<int, int> index_map;
+    
+  for (int i = 0; i < nums.size(); i++) {
+    //index_map[nums[i]] = i; //insert the number and index
+    index_map.insert({nums[i],i}); //insert the number and index
+    int diff = target - nums[i];
+    if (index_map.count(diff) > 0) {
+      if (index_map[diff] != i) {
+	pair.push_back(index_map[diff]);
+	pair.push_back(i);
+	return(pair);
+      }
+    }
   }
+  return(pair);
+}
 
+int main (int argc, char **argv) {
+
+  vector<int> numbers = {3,3};
+  vector<int> pair = twoSum (numbers, 6);
+  std::cout << pair[0] << " " << pair[1] << "\n";
 }
